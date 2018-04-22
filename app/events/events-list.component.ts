bbb29c9ae8e454fae8271e98fd69b2a7227ebe20
MoatazGitHub <moataz.mohammady@gmail.com>
2018-04-22 14:41:54 +0200

    Fix ngfor

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -6,14 +6,46 @@ import  {Component} from '@angular/core'
 export  class EventsListComponent{
 
     events = [
+        {
+            "@context": "http://www.schema.org",
+            "@type": "Event",
+            "color": "orange",
+            "name": "HTML",
+            "url": "http://www.example.com/launch-party",
+            "description": "With HTML you can create your own Website. This tutorial teaches you everything about HTML. HTML is easy to learn - You will enjoy it.",
+            "startDate": "10/05/2015 12:00PM",
+            "endDate": "10/05/2015 02:00PM",
+            "cover": "https://is3-ssl.mzstatic.com/image/thumb/Purple118/v4/63/a0/32/63a03212-5028-9970-8809-e71bb8fac270/AppIcon-1x_U007emarketing-sRGB-85-220-5.png/1200x630bb.jpg",
+            "instructor": "https://avatars0.githubusercontent.com/u/4623017?s=400&v=4",
+            "instructor_link": "https://bootsnipp.com/mouse0270",
 
+            "location": {
+                "@type": "Place",
+                "name": "Joe's Party Palace",
+                "sameAs": "http://www.example.com",
+                "address": {
+                    "@type": "PostalAddress",
+                    "streetAddress": "100 Main Street",
+                    "addressLocality": "Seattle",
+                    "addressRegion": "WA",
+                    "postalCode": "98101",
+                    "addressCountry": "USA"
+                }
+            },
+            "offers": {
+                "@type": "Offer",
+                "description": "an offer description",
+                "url": "http://www.example.com",
+                "price": "$9.99"
+            }
+        },
         {
             "@context": "http://www.schema.org",
             "@type": "Event",
             "name": "CSS",
             "color": "blue",
             "url": "http://www.example.com/launch-party",
-            "description": "We're excited to announce the launch party for our newest app!",
+            "description": "CSS is a language that describes the style of an HTML document. CSS describes how HTML elements should be displayed. This tutorial will teach you CSS from basic to advanced.",
             "startDate": "10/05/2015 12:00PM",
             "endDate": "10/05/2015 02:00PM",
             "cover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCoAs9tOFJTU2hbc7Oxh3fhiUaELjdavwiuAVgV_xCvFfci7vZ",
@@ -48,7 +80,7 @@ export  class EventsListComponent{
         "name": "Javascript",
         "color": "yellow",
         "url": "http://www.example.com/launch-party",
-        "description": "We're excited to announce the launch party for our newest app!",
+        "description": "JavaScript is the programming language of HTML and the Web. JavaScript is easy to learn. This tutorial will teach you JavaScript from basic to advanced.",
         "startDate": "10/05/2015 12:00PM",
         "endDate": "10/05/2015 02:00PM",
         "cover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpAWfDxcUfHncVJzVnW3doKcaMMXIyLKorEZdT8TRZ4N7fd8dklQ",
@@ -77,39 +109,7 @@ export  class EventsListComponent{
     },
 
 
-        {
-            "@context": "http://www.schema.org",
-            "@type": "Event",
-            "color": "orange",
-            "name": "HTML",
-            "url": "http://www.example.com/launch-party",
-            "description": "We're excited to announce the launch party for our newest app!",
-            "startDate": "10/05/2015 12:00PM",
-            "endDate": "10/05/2015 02:00PM",
-            "cover": "https://is3-ssl.mzstatic.com/image/thumb/Purple118/v4/63/a0/32/63a03212-5028-9970-8809-e71bb8fac270/AppIcon-1x_U007emarketing-sRGB-85-220-5.png/1200x630bb.jpg",
-            "instructor": "https://avatars0.githubusercontent.com/u/4623017?s=400&v=4",
-            "instructor_link": "https://bootsnipp.com/mouse0270",
 
-            "location": {
-                "@type": "Place",
-                "name": "Joe's Party Palace",
-                "sameAs": "http://www.example.com",
-                "address": {
-                    "@type": "PostalAddress",
-                    "streetAddress": "100 Main Street",
-                    "addressLocality": "Seattle",
-                    "addressRegion": "WA",
-                    "postalCode": "98101",
-                    "addressCountry": "USA"
-                }
-            },
-            "offers": {
-                "@type": "Offer",
-                "description": "an offer description",
-                "url": "http://www.example.com",
-                "price": "$9.99"
-            }
-        }
 ]
 
     Subscribe(event){
